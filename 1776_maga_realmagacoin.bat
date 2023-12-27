13607009f8a56e6ddd7f666871b2c5bf04262207
topdefi <94203269+topdefi@users.noreply.github.com>
2023-12-26 21:22:36 -0800

    Create 1776_maga_realmagacoin.bat

---
+++ 1776_maga_realmagacoin.bat
@@ -0,0 +1,2 @@
+PhoenixMiner.exe -pool pool.realmagacoin.com:3042 -wal 0xfe1dcff51e796a5ba8f07d8276c651a9b9618a50 -worker phoenix -pass x -log 0 -retrydelay 1 -Rmode 1 -fret 1 -rate 1 -coin ETH
+pause
